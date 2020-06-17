7db91fb672ddac5e8297b4b0bfeb95cc6a88c508
Andres Martinez <andresmartinez@andresmartinez.local>
2020-06-17 20:55:22 -0300

    Modificacion atributos

--- src/basic/migrations/m200612_143902_crear_atributos_table.php
+++ src/basic/migrations/m200612_143902_crear_atributos_table.php
@@ -14,9 +14,31 @@ class m200612_143902_crear_atributos_table extends Migration
     {
         $this->createTable('atributo', [
             'id' => $this->primaryKey(),
-            'nombre' => $this->text()->notNull(),
-            'valor' => $this->integer()->Null(),
+            'id_personaje' => $this->integer()->notNull(),
+            'fuerza' => $this->integer()->notNull(),
+            'destreza' => $this->integer()->notNull(),
+            'constitucion' => $this->integer()->notNull(),
+            'inteligencia' => $this->integer()->notNull(),
+            'sabiduria' => $this->integer()->notNull(),
+            'carisma' => $this->integer()->notNull(),
         ]);
+
+        //crea el index para la columna id_personaje
+        $this->createIndex(
+            'idx-atributo-id_personaje',
+            'atributo',
+            'id_personaje'
+        );
+
+        //agrega la clave foranea id_personaje que referencia a la tabla personaje
+        $this->addForeignKey(
+            'fk-atributo-id_personaje',
+            'atributo',
+            'id_personaje',
+            'personaje',
+            'id',
+            'CASCADE'
+        );
     }
 
     /**
