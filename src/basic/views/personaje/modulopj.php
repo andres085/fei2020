484e39544495f2f073f1fbcbc44419f4d68f9e43
Marcus5114 <fentraigas5114@gmail.com>
2020-08-14 12:51:02 -0300

    Revert "modulo pj"
    This reverts commit 8eafaa29542faebc1d28869777bca6ede7db39eb.

--- src/basic/views/personaje/modulopj.php
+++ src/basic/views/personaje/modulopj.php
@@ -14,7 +14,60 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", ['
     <div id="app">
         <h1 style="text-align: center;"> Módulo del Jugador</h1>
         <br>
-        <div class="row">
+
+        <!-- PRUEBA TOAST 
+        <button type="button" id="btnToast" class="btn btn-block btn-success">Mostrar Toast</button>
+
+        <div class="toast" data-delay="2000" id="toast" style="position: absolute; bottom: 0; right: 0;">
+            <div class="toast-header">
+                Toast Header
+            </div>
+            <div class="toast-body">
+            Some text inside the toast body
+            </div>
+        </div>
+    FIN PRUEBA TOAST -->
+<div class="row">
+
+
+    <div class="col-md-12">
+                    
+        <button class="btn btn-block btn-outline-dark" onclick="window.location.href='creadorpj1'"><h2><i class="fas fa-plus"></i><br> Agregar Personaje</h2></button>
+        
+        <div style="text-align: center;">
+            <hr>
+
+            <h3>Tus Personajes</h3>
+            <h3><i class="fas fa-users"></i></h3>
+            
+            <div class="container-fluid" style="max-height:450px ;overflow-y: auto;">
+
+            <button type="button" class="btn btn-outline-dark btn-pjs" data-toggle="modal" data-target="#ModalPj">Personaje <h6 style="font-size:smaller;">Raza - Clase - Nivel</h6>
+            <button type="button" class="btn btn-outline-dark btn-pjs" data-toggle="modal" data-target="#ModalPj">Personaje <h6 style="font-size:smaller;">Raza - Clase - Nivel</h6>
+            <button type="button" class="btn btn-outline-dark btn-pjs" data-toggle="modal" data-target="#ModalPj">Personaje <h6 style="font-size:smaller;">Raza - Clase - Nivel</h6>
+            <button type="button" class="btn btn-outline-dark btn-pjs" data-toggle="modal" data-target="#ModalPj">Personaje <h6 style="font-size:smaller;">Raza - Clase - Nivel</h6>
+            <button type="button" class="btn btn-outline-dark btn-pjs" data-toggle="modal" data-target="#ModalPj">Personaje <h6 style="font-size:smaller;">Raza - Clase - Nivel</h6>
+            <button type="button" class="btn btn-outline-dark btn-pjs" data-toggle="modal" data-target="#ModalPj">Personaje <h6 style="font-size:smaller;">Raza - Clase - Nivel</h6>
+            <button type="button" class="btn btn-outline-dark btn-pjs" data-toggle="modal" data-target="#ModalPj">Personaje <h6 style="font-size:smaller;">Raza - Clase - Nivel</h6>
+            <button type="button" class="btn btn-outline-dark btn-pjs" data-toggle="modal" data-target="#ModalPj">Personaje <h6 style="font-size:smaller;">Raza - Clase - Nivel</h6>
+           
+            
+            </div>
+        </div>
+                    
+    </>
+
+   
+
+
+    <!-- <script>
+        $(document).ready(function(){
+            $("#btnToast").click(function(){
+                $('#toast').toast('show');
+            });
+        });
+    </script> -->
+
 
             <div class="col-md-12">
 
@@ -40,8 +93,6 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", ['
         </div>
     </div>
 </div>
-
-
 <script>
     var app = new Vue({
         el: '#app',
