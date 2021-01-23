91182f4308f7f5560fe3cdaf8a3110f735536218
Andres Martinez <andresmartinez@andresmartinez.local>
2021-01-23 20:18:04 -0300

    Cambios al first-start

--- bin/first-start.sh
+++ bin/first-start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+docker-compose exec app composer global require "fxp/composer-asset-plugin:^1.4.1"
 
 docker-compose exec app composer install
-docker-compose exec app chmod 777 ./web/assets -R
+docker-compose exec app chmod 777 ./web/assets -R 
 docker-compose exec app chmod 777 ./runtime -R
+
+docker-compose exec app yii migrate
